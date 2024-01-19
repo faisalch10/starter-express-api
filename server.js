0d4423ae91178775f1891d631e408b00f61bf96e
HouseOfCoding <houseofcoding10@gmail.com>
2024-01-20 00:50:29 +0500

    third commmit

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use('/api/v1/auth', authRoutes);
 // Error handling middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is up and running on PORT ${PORT}`);
 });
